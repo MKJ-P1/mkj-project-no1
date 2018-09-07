de729ac48da173a36e94ef76c2075198ec86823f
BJ Choi <byungjun.choi@resolatte.com>
2018-09-07 16:09:11 +0900

    20180907 - Messaging Service

--- functions/index.js
+++ functions/index.js
@@ -7,15 +7,9 @@ app.engine('hbs', engines.handlebars);
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
-const facts = [
-  {text: "1+1 = 2"},
-  {text: "真実はいつも一つ"},
-  {text: "工藤新一はコナン"}
-];
-
 app.get('/', (request, response) => {
   response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-  response.render('index', { facts });
+  response.render('index');
 })
 
 exports.app = functions.https.onRequest(app);
