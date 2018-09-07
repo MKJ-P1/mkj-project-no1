e00365238e5b023e0cfb6a5cea7c8860b9cea38d
BJ Choi <byungjun.choi@resolatte.com>
2018-09-07 12:44:23 +0900

    20180907 - Firebase Setting4

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const engines = require('consolidate');
 
 const app = express();
-app.get('/timestamp', (request, response) => {
-  response.send(`${Date.now()}`);
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+const facts = [
+  {text: "1+1 = 2"},
+  {text: "真実はいつも一つ"},
+  {text: "工藤新一はコナン"}
+];
+
+app.get('/', (request, response) => {
+  response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+  response.render('index', { facts });
 })
 
 exports.app = functions.https.onRequest(app);
