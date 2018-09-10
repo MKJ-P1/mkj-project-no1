c7b7844552b221fe94a5e74f6e30ef75c9813348
BJ Choi <byungjun.choi@resolatte.com>
2018-09-10 18:14:07 +0900

    20180910 - Style Update

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,14 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const engines = require('consolidate');
-
 const app = express();
+
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
 app.set('view engine', 'hbs');
-
 app.get('/', (request, response) => {
-  response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-  response.render('index');
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+    response.render('index');
 })
 
 exports.app = functions.https.onRequest(app);
